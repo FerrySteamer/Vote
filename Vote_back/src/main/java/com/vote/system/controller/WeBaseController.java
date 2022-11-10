caeaaafaef18830f2d3c5232a8928a6549fd0390
ProteinCode <91516888+FerrySteamer@users.noreply.github.com>
2022-11-10 20:14:38 +0800

    WeBaseService

--- Vote_back/src/main/java/com/vote/system/controller/WeBaseController.java
+++ Vote_back/src/main/java/com/vote/system/controller/WeBaseController.java
@@ -1,5 +1,11 @@
 package com.vote.system.controller;
 
+import cn.hutool.core.lang.Dict;
+import com.vote.system.service.WeBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,5 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class WeBaseController {
+    @Autowired
+    WeBaseService service;
 
+    @RequestMapping(value="/login",method = RequestMethod.POST)
+    public Dict haveAccount(@RequestParam("account") String account) {
+        return service.haveAccount(account);
+    }
+
+    @RequestMapping(value="/myself",method = RequestMethod.POST)
+    public Dict getAllproject(@RequestParam("account") String account) {
+        return service.getAllproject(account);
+    }
 }
--- Vote_back/src/main/java/com/vote/system/service/WeBaseService.java
+++ Vote_back/src/main/java/com/vote/system/service/WeBaseService.java
@@ -6,21 +6,35 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.json.*;
 import com.vote.system.utils.IOUtil;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
+@Service
 public class WeBaseService {
-    @Value("${system.contract.srinGetSetAddress}")
+    @Value("${system.contract.VoteAddress}")
     String contractAddress;
 
     @Value("${fisco.server.url}")
     String fiscoUrl;
 
-    public static final String ABI = IOUtil.readResourceAsString("abi/StringGetSet.abi");
+    public static final String ABI = IOUtil.readResourceAsString("com/vote/system/abi/Vote.abi");
 
-    public Dict newGet(String userAddress) {
+    public static String userAddress = "";
+
+    public Dict haveAccount(String account) {
+        userAddress = account;
+
         List funcParam = new ArrayList();
-        Dict result = commonReq(userAddress,"get",funcParam);
+        funcParam.add(account);
+        Dict result = commonReq(userAddress,"haveAccount",funcParam);
+        return result;
+    }
+
+    public Dict getAllproject(String account) {
+        List funcParam = new ArrayList();
+        Dict result = commonReq(account,"getAllproject",funcParam);
         return result;
     }
 
@@ -29,11 +43,12 @@ public class WeBaseService {
         JSONObject data = JSONUtil.createObj();
         data.set("groupId","1");
         data.set("user",userAddress);
-        data.set("contractName","StringGetSet");
+        data.set("contractName","Vote");
         data.set("version","");
         data.set("funcName",funcName);
         data.set("funcParam",funcParam);
         data.set("contractAddress",contractAddress);
+        data.set("contractAbi",abiJSON);
         data.set("useAes",false);
         data.set("useCns",false);
         data.set("cnsName","");
@@ -49,7 +64,37 @@ public class WeBaseService {
         return retDict;
     }
 
-    
+    public Dict listDeployedContract() {
+        JSONObject data = JSONUtil.createObj();
+        data.set("groupId","1");
+        data.set("contractName","");
+        data.set("contractStatus",2);
+        data.set("contractAddress","");
+        data.set("pageNumber",1);
+        data.set("pageSize",10);
 
+        String dataString = JSONUtil.toJsonStr(data);
+        String responseBody = HttpRequest.post(fiscoUrl+"/WeBASE-Front/contract/contractList")
+                .header(Header.CONTENT_TYPE,"application/json")
+                .body(dataString)
+                .execute()
+                .body();
+        JSONObject bodyJSON = JSONUtil.parseObj(responseBody);
+        JSONArray constractArray = JSONUtil.parseArray(bodyJSON.get("data"));
+        List<Object> retArr = constractArray.stream().map(obj -> {
+            JSONObject rawObj = (JSONObject) obj;
+            JSONObject json = new JSONObject();
+            json.set("合约名称",rawObj.get("contractName"));
+            json.set("合约地址",rawObj.get("contractAddress"));
+            json.set("部署时间",rawObj.get("deployTime"));
+            json.set("创建时间",rawObj.get("createTime"));
+            json.set("修改时间",rawObj.get("ModifyTime"));
+            json.set("abi",rawObj.get("contractAbi"));
+            return json;
+        }).collect(Collectors.toList());
 
+        Dict retDict = new Dict();
+        retDict.set("result",retArr);
+        return retDict;
+    }
 }
